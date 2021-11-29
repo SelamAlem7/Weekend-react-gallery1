df41158706a5dcd41a69bf6bd797ef712ed54b66
Selam Alem <selamtalem@gmail.com>
2021-11-28 18:29:58 -0600

    done copy

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,7 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 2, path: 'images/family.jpg', description: 'Photo that symbolized the importance of family to me', likes: 0 },
+    { id: 3, path: 'images/religion.jpg', description: 'Photo of an Ethiopian Orthodox Christian Cross symbolizing the importance of my religion in my life ', likes: 0 }
 ];
 
 module.exports = galleryItems;
